d996b82921218fa03e2b034444960c1d1fb0800e
Rafael Krysciak <rafael.krysciak@gmail.com>
2016-10-03 15:18:51 +0200

    Keep files on all nodes for 2 days instead of 4

--- config/hqm.php
+++ config/hqm.php
@@ -2,7 +2,7 @@
 
 return [
     'database_cleanup_older_then' => '2 WEEK',
-    'keep_files_on_all_nodes_days' => 4,
+    'keep_files_on_all_nodes_days' => 2,
     'download_files_not_older_then_days' => 7,
     'retry_download_after_minutes' => 30
 ];
